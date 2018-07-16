7c99597ef2616b1a3ab989bb75bb11836df3ad8d
Robert Tice <tice@adacore.com>
2018-07-16 10:21:23 -0400

    Adding scroll to top feature.

--- engine/sphinx/learn_theme/static/js/customizer.js
+++ engine/sphinx/learn_theme/static/js/customizer.js
@@ -42,3 +42,20 @@ $( function() {
     });
 
 });
+
+var amountScrolled = 300;
+
+$(window).scroll( function() {
+    if ( $(window).scrollTop() > amountScrolled ) {
+        $( "#scrollToTopBtn" ).fadeIn('slow');
+    } else {
+        $( "#scrollToTopBtn" ).fadeOut('slow');
+    }
+});
+
+$( "#scrollToTopBtn" ).click(function() {
+    $( 'html, body' ).animate({
+        scrollTop: 0
+    }, 300 );
+    return false;
+});
