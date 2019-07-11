9b038f765c375829e623cd44c0ddddfb4d681371
Robert Tice <tice@adacore.com>
2019-07-11 13:47:53 -0400

    Fixing alignment of custom input checkbox and label.

--- engine/sphinx/learn_theme/static/js/editors.js
+++ engine/sphinx/learn_theme/static/js/editors.js
@@ -590,7 +590,9 @@ function fill_editor_from_contents(container, example_server, resources) {
 
     if(container.attr("cli_input")) {
         $( '<textarea class="custom_input" name="custom_input" rows="4" cols="6"></textarea>' ).appendTo(buttons_div);
-        var custom_check = $( '<input type="checkbox" class="custom_check" name="custom_check"><label for="custom_check">Test against custom input</label>' ).appendTo(buttons_div).change( function() {
+        var unique_id = generateUniqueId();
+        var div = $('<div class="custom_check_container">').appendTo(buttons_div);
+        var custom_check = $( '<input type="checkbox" id="' + unique_id + ' class="custom_check"><label class="custom_check" for="' + unique_id + '">Test against custom input</label>' ).appendTo(div).change( function() {
             var input_search = container.find( 'textarea[name="custom_input"]' );
             if ($(this).is(':checked')) {
                 if(input_search.length == 1) {
