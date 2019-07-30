f25e3b1683d01e708c0dd9832de3cd712fa4483b
Robert Tice <tice@adacore.com>
2019-07-30 17:42:11 -0400

    Adding new google tag manager stuff.

--- engine/sphinx/learn_theme/static/js/adacore_analytics.js
+++
@@ -1,9 +0,0 @@
-(function() {
-  if (!Cookies.get('AdaCore_staff')) {
-    window.dataLayer = window.dataLayer || [];
-    function gtag(){dataLayer.push(arguments);}
-    gtag('js', new Date());
-
-    gtag('config', 'UA-117428-14');
-  }
-})();
