84ef6bfd72f7b53520305cad79fd66b93bae2700
gusthoff <gusthoff@gusthoff.com>
2019-11-08 20:23:50 +0100

    Sphinx: using year as release version

--- engine/sphinx/conf.py
+++ engine/sphinx/conf.py
@@ -27,7 +27,7 @@ title = u'Learn Ada (Complete)'
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'1'
+release = u'2019'
 
 
 # -- General configuration ---------------------------------------------------
