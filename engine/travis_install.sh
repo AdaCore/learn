4d8beb60ad241b2ed570089cf374feabcbc74a89
gusthoff <gusthoff@gusthoff.com>
2019-11-29 19:19:15 +0100

    Travis: fixed directory creation
    Fixed issue when updating GNAT Community version

--- engine/travis_install.sh
+++ engine/travis_install.sh
@@ -8,13 +8,25 @@ set -e
 
 package_file=5cdffc5409dcd015aaf82626
 
+# Create tools directory
+if ! test -d $TOOLS_DIR
+then
+    mkdir -p $TOOLS_DIR
+fi
+
+# Get / update gnat_community_install_script repo
+if ! test -d $TOOLS_DIR/gnat_community_install_script
+then
+    (cd $TOOLS_DIR && git clone https://github.com/AdaCore/gnat_community_install_script)
+else
+    (cd $TOOLS_DIR/gnat_community_install_script && git pull)
+fi
+
 # Get GNAT
 if ! test -f $GNAT_INSTALLER_PATH
 then
-    mkdir -p $TOOLS_DIR
     wget -O $GNAT_INSTALLER_PATH \
         "http://mirrors.cdn.adacore.com/art/${package_file}"
-    (cd $TOOLS_DIR && git clone https://github.com/AdaCore/gnat_community_install_script)
 fi
 
 # If needed, extract GNAT
