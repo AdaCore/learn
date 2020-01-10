123763b9e82e1cfe9c02000588ee7e89a241db5d
Robert Tice <tice@adacore.com>
2020-01-10 14:42:31 -0500

    Adding deps for typescript unit testing. Updating nodejs to v12 from v8. Updating npm deps.

--- frontend/bootstrap.sh
+++ frontend/bootstrap.sh
@@ -1,24 +1,25 @@
 #!/usr/bin/env bash
 
+# Enable the NodeSource repository
+curl -sL https://deb.nodesource.com/setup_10.x | sudo -E bash -
+
+# Add yarn to apt-get
+curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
+
 # Install system deps
 apt-get update
 apt-get install -y \
     python3 \
     python3-pip \
     nodejs \
-    npm \
     graphviz \
     make \
-    gnat
+    gnat \
+    yarn
 
 # Install learn deps
 cd /vagrant/frontend
 pip3 install -r requirements.txt
-
-# Install yarn
-apt remove cmdtest
-curl -sS https://dl.yarnpkg.com/debian/pubkey.gpg | apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | tee /etc/apt/sources.list.d/yarn.list
-apt-get update && apt-get install yarn
 
 yarn
