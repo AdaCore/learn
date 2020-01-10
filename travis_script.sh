319317332998fd6ccf87478d469289f1683a88cd
Robert Tice <tice@adacore.com>
2020-01-10 10:38:19 -0500

    Debugging travis build failure in travis_script.sh.

--- travis_script.sh
+++ travis_script.sh
@@ -2,11 +2,15 @@
 
 export PATH=$GNAT_PATH/bin:$PATH
 
+set -v
+set -e
+
 which gcc
 which gnatchop
 which gprbuild
 
 cd frontend
+ls
 make SPHINXOPTS="-W" tests
 # TODO: need to build the frontend to test build
 
