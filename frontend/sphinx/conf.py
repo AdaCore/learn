0de04ede3234abbaabfd1d862ac878f69cda710e
Robert Tice <tice@adacore.com>
2020-03-23 12:52:14 -0400

    Fixing various console issues: missing favicons and undefined references to js functions.

--- frontend/sphinx/conf.py
+++ frontend/sphinx/conf.py
@@ -129,7 +129,7 @@ html_show_sourcelink = False
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-# html_static_path = ["dist"]
+html_static_path = ['img',]
 
 # Custom sidebar templates, must be a dictionary that maps document names
 # to template names.
