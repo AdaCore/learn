438e7a140d3f325db0f57ac92b102bbffc488744
Robert Tice <tice@adacore.com>
2020-03-27 10:17:37 -0400

    Updating footer to include copyright year of the build date.

--- frontend/sphinx/conf.py
+++ frontend/sphinx/conf.py
@@ -12,6 +12,7 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
+import datetime
 import os
 # import sys
 # sys.path.insert(0, os.path.abspath('.'))
@@ -152,6 +153,10 @@ html_static_path = ['img',]
 # html_sidebars = {}
 
 html_copy_source = False
+
+html_context = {
+    'year': datetime.date.today().strftime('%Y'),
+}
 
 
 # -- Options for HTMLHelp output ---------------------------------------------
