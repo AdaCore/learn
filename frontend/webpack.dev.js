7cb6bb919531f83e37317dfb0045e54904425bfd
Robert Tice <tice@adacore.com>
2020-04-16 15:34:00 -0400

    Fixing webpack script syntax to allow for more future flexibility.

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -5,28 +5,34 @@ const WatchPlugin = require('webpack-watch-files-plugin').default;
 const ShellPlugin = require('webpack-shell-plugin');
 const common = require('./webpack.common.js');
 
-module.exports = env => merge(common(env), {
-  mode: 'development',
-  watch: true,
-  devServer: {
-    contentBase: [path.join(__dirname, 'dist', 'html'), path.join(__dirname, 'dist')],
-    watchContentBase: true,
-    compress: false,
-    host: '0.0.0.0',
-    port: 8080,
-    hot: true,
-    liveReload: false,
-    index: 'index.html',
-    publicPath:  '/_static/'
-  },
-  devtool: 'source-map',
-  plugins: [
-    new ShellPlugin({
-      onBuildStart: ['make cleanall -j4'],
-      onBuildEnd: ['make local -j4'],
-      // dev=false here to force every build to trigger make, the default is
-      // first build only.
-      dev: false,
-    }),
-  ]
-});
+module.exports = function(env) {
+  const common_config = common(env);
+
+  const dev_config = {
+    mode: 'development',
+    watch: true,
+    devServer: {
+      contentBase: [path.join(__dirname, 'dist', 'html'), path.join(__dirname, 'dist')],
+      watchContentBase: true,
+      compress: false,
+      host: '0.0.0.0',
+      port: 8080,
+      hot: true,
+      liveReload: false,
+      index: 'index.html',
+      publicPath:  '/_static/'
+    },
+    devtool: 'source-map',
+    plugins: [
+      new ShellPlugin({
+        onBuildStart: ['make cleanall -j4'],
+        onBuildEnd: ['make local -j4'],
+        // dev=false here to force every build to trigger make, the default is
+        // first build only.
+        dev: false,
+      }),
+    ]
+  };
+
+  return merge(common_config, dev_config);
+}
