9fb61119f1ac65037531c991639d91dc33343b06
gusthoff <gusthoff@gusthoff.com>
2020-04-17 16:50:36 +0200

    Revert "Backend: adding "-gnata" switch to the list"
    This reverts commit 2c17fec50b85536caac75ca6b9fb88b0342f37b9.

--- backend/app/widget/project.py
+++ backend/app/widget/project.py
@@ -252,10 +252,10 @@ class RemoteProject(Project):
             Returns the status code returned from the build
         """
         rep = MQReporter(self.app, self.task_id)
-        rep.console(["gprbuild", "-q", "-P", self.gpr.get_name(), "-gnatwa", "-gnata"])
+        rep.console(["gprbuild", "-q", "-P", self.gpr.get_name(), "-gnatwa"])
 
         main_path = os.path.join(self.remote_tempd, self.gpr.get_name())
-        line = ["gprbuild", "-q", "-P", main_path, "-gnatwa", "-gnata"]
+        line = ["gprbuild", "-q", "-P", main_path, "-gnatwa"]
 
         code, out, err = self.container.execute(line, rep)
         if code != 0:
