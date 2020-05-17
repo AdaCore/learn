4372b19c00545cd153720eb574010e95230d1068
gusthoff <gusthoff@gusthoff.com>
2020-05-18 00:33:43 +0200

    Editorial change: updating release date

--- frontend/sphinx/conf.py
+++ frontend/sphinx/conf.py
@@ -24,7 +24,7 @@ import sys
 # -- Project information -----------------------------------------------------
 
 project = u'learn.adacore.com'
-copyright = u'2019, AdaCore'
+copyright = u'2018 – 2020, AdaCore'
 author = u'AdaCore' if 'SPHINX_AUTHOR' not in os.environ else \
     os.environ['SPHINX_AUTHOR']
 title = u'Learn Ada (Complete)' if 'SPHINX_TITLE' not in os.environ else \
@@ -33,7 +33,7 @@ title = u'Learn Ada (Complete)' if 'SPHINX_TITLE' not in os.environ else \
 # The short X.Y version
 version = u''
 # The full version, including alpha/beta/rc tags
-release = u'2020-01'
+release = u'2020-05'
 
 
 # -- General configuration ---------------------------------------------------
