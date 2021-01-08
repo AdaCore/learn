a412891aa6e725657ed8f3a2d2cb2ca55e58c73e
gusthoff <gusthoff@gusthoff.com>
2021-01-08 20:07:20 +0100

    Adding upcoming "Ada for the C Embedded Developer" course to the list.

--- content/courses/courses.rst
+++ content/courses/courses.rst
@@ -13,5 +13,6 @@ Courses
    Introduction to Ada <intro-to-ada/index>
    Introduction to SPARK <intro-to-spark/index>
    Ada for the C++ or Java Developer <Ada_For_The_CPP_Java_Developer/index>
+   Ada for the C Embedded Developer <Ada_For_The_C_Embedded_Developer/index>
    SPARK Ada for the MISRA C Developer <SPARK_for_the_MISRA_C_Developer/index>
    Introduction to GNAT Toolchain <GNAT_Toolchain_Intro/index>
--- frontend/sphinx/conf.py
+++ frontend/sphinx/conf.py
@@ -100,7 +100,6 @@ exclude_patterns = [u'_build',
 # Exclude internal and unfinished material from final site build
 if 'GEN_LEARN_SITE' in os.environ and os.environ['GEN_LEARN_SITE'] == "yes":
     exclude_patterns += ['**internal/**',
-                         '**courses/Ada_For_The_C_Embedded_Developer/**',
                          '**courses/intro-to-embedded-sys-prog/**',
                          '**courses/advanced-ada/**',
                          '**courses/advanced-spark/**']
