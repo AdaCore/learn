587141f6360c191f65229fce5d9a1d95555a38df
gusthoff <gusthoff@gusthoff.com>
2021-09-10 23:58:02 +0200

    Sphinx: restricting templates to HTML output

--- frontend/sphinx/conf.py
+++ frontend/sphinx/conf.py
@@ -75,7 +75,7 @@ extensions = [
 ]
 
 # Add any paths that contain templates here, relative to this directory.
-templates_path = ['_templates']
+templates_path = []
 
 # The suffix(es) of source filenames.
 # You can specify multiple suffix as a list of string:
@@ -353,6 +353,8 @@ def setup(app):
 
     # TODO: find a better way to retrieve the current target (html/latex/epub)
     if 'html' in app.outdir:
+        templates_path.append('_templates')
+
 
         if not os.getenv('FRONTEND_TESTING'):
             try:
