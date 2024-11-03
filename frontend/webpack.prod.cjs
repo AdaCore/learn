92b201652c087e1f7449a8eb3da806b5a2a611a4
gusthoff <gusthoff@gusthoff.com>
2024-11-03 01:06:43 +0100

    TS frontend: rename files using '.csj' file extension
    - Indicating that these files are still CommonJS modules.
    - Fixing issue with ESM system.

--- frontend/webpack.prod.cjs
+++ frontend/webpack.prod.cjs
@@ -1,7 +1,7 @@
 const { merge } = require('webpack-merge');
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
 const TerserPlugin = require('terser-webpack-plugin');
-const common = require('./webpack.common.js');
+const common = require('./webpack.common.cjs');
 
 const terser_config = function(env) {
   const staging = (env && env.staging);
