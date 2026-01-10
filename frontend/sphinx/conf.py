d241bc49e24a679136d5cca0c55800e5ebe3e42d
gusthoff <gusthoff@gusthoff.com>
2026-01-10 20:55:11 +0100

    Editorial change: updating copyright year

--- frontend/sphinx/conf.py
+++ frontend/sphinx/conf.py
@@ -56,7 +56,7 @@ def get_file_from_conf_ini(path_to_file):
 # -- Project information -----------------------------------------------------
 
 project = u'learn.adacore.com'
-copyright = u'2018 – 2025, AdaCore. All rights reserved'
+copyright = u'2018 – 2026, AdaCore. All rights reserved'
 author = u'AdaCore' if not config.has_option('', 'author') else \
     config['DEFAULT']['author']
 title = u'Learn Ada (Complete)' if not config.has_option('', 'title') else \
